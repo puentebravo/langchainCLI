3845857024f1ead7db6fa475fd3fb83c094dfcad
Puentebravo <puentematos@gmail.com>
2023-07-26 19:48:20 -0400

    Added structuredParser, modified prompt to generate a workout of a requested length

--- index.ts
+++ index.ts
@@ -1,34 +1,50 @@
 import inquirer from "inquirer";
 import { OpenAI } from "langchain/llms/openai";
 import { PromptTemplate } from "langchain/prompts";
+import { StructuredOutputParser } from "langchain/output_parsers";
 
 require("dotenv").config();
 
+interface inquirerResult {
+  prompt: string
+}
 const model = new OpenAI({
   openAIApiKey: process.env.OPENAI_API_KEY,
   temperature: 0,
   modelName: "gpt-3.5-turbo",
 });
 
+const parser = StructuredOutputParser.fromNamesAndDescriptions({
+  workout: "Individual exercises for the workout",
+  length: "the length in minutes of the workout",
+  summary: "detailed directions on how to execute each exercise",
+});
+
+const formatInstructions = parser.getFormatInstructions();
+
 const promptFunction = async (input: string) => {
   try {
-
     const prompt = new PromptTemplate({
-        template: "As a JavaScript expert, please answer the user's coding questions as thoroughly as possible. \n {question}",
-        inputVariables: ["question"]
-    })
+      template:
+        "You are a fitness expert. Please give the user a workout of the length they specify. \n{format_instructions}  \n {time}",
+      inputVariables: ["time"],
+      partialVariables: { format_instructions: formatInstructions },
+    });
 
     const promptInput = await prompt.format({
-        question: input
-    })
+      time: input,
+    });
 
     const res = await model.call(promptInput);
 
-    console.log(res);
+    const parsedRes = await parser.parse(res)
+
+    console.log(parsedRes);
   } catch (err) {
     console.error(err);
   }
 };
+
 
 const init = () => {
   inquirer
@@ -36,10 +52,10 @@ const init = () => {
       {
         type: "input",
         name: "prompt",
-        message: "Please input a question:",
+        message: "How much time do you have for a workout?",
       },
     ])
-    .then((data) => {
+    .then((data: inquirerResult) => {
       promptFunction(data.prompt);
     });
 };
